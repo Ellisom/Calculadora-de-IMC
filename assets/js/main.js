09b82e14616008c7324a42088f442b3d82af0e2e
Ellisom <ellisomedeiros@gmail.com>
2022-11-03 01:38:53 -0300

    Alteração em function e css

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,38 +12,36 @@
         const resultadoIMC = getIMC(pesoPessoa, alturaPessoa);
 
         if (resultadoIMC < 18.5) {
-            setResultado(`Seu IMC é ${resultadoIMC} (Abaixo do peso)`, true);
-            resultado.style.backgroundColor = 'rgb(86, 233, 142)';
-            
+            setResultado(`Seu IMC é ${resultadoIMC} (Abaixo do peso)`, true);       
         }
+
         else if (resultadoIMC >= 18.5 && resultadoIMC <= 24.9) {
             setResultado(`Seu IMC é ${resultadoIMC} (Peso normal)`, true);
-            resultado.style.backgroundColor = 'rgb(86, 233, 142)';
         }
+
         else if (resultadoIMC >= 25 && resultadoIMC <= 29.9) {
             setResultado(`Seu IMC é ${resultadoIMC} (Sobrepeso)`, true);
-            resultado.style.backgroundColor = 'rgb(86, 233, 142)';
         }
+
         else if (resultadoIMC >= 30 && resultadoIMC <= 34.9) {
             setResultado(`Seu IMC é ${resultadoIMC} (Obesidade grau 1)`, true);
             resultado.style.backgroundColor = 'rgb(86, 233, 142)';
         }
+        
         else if (resultadoIMC >= 35 && resultadoIMC <= 39.9) {
             setResultado(`Seu IMC é ${resultadoIMC} (Obesidade grau 2)`, true);
-            resultado.style.backgroundColor = 'rgb(86, 233, 142)';
         }
+
         else if (resultadoIMC > 40) {
             setResultado(`Seu IMC é ${resultadoIMC} (Obesidade grau 3)`, true);
-            resultado.style.backgroundColor = 'rgb(86, 233, 142)';
         } 
 
         else if (!pesoPessoa) {
             setResultado('Peso inválido.', false);
-            resultado.style.backgroundColor = 'rgb(212, 78, 78)';
         } 
+        
         else if (!alturaPessoa) {
             setResultado('Altura inválida.', false);
-            resultado.style.backgroundColor = 'rgb(212, 78, 78)';
         }
     }
 
@@ -64,6 +62,12 @@
     function setResultado(msg, isValid) {
         const p = criaP();
         resultado.innerHTML = '';
+
+        if (isValid) {
+            p.classList.add('paragrafo-verde');
+        } else {
+            p.classList.add('paragrafo-vermelho');
+        }
         p.innerHTML = msg;
         resultado.appendChild(p);
         //Insere o elemento <p> criado através da function criaP() na div resultado selecionada através de querySelector('.resultado');
